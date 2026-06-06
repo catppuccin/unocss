f71d7a2825ef05c8200651065c71dfdc2c4e406e
tuhana <tuhana.cat+github@gmail.com>
2026-06-06 20:15:18 +0300

    fix(example): build (#267)

--- example/astro.config.ts
+++ example/astro.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import UnoCSS from 'unocss/astro';
+import UnoCSS from '@unocss/astro';
 
 export default defineConfig({
   site: 'https://unocss.catppuccin.org',
